258022fb5967312278fe284293e5b0377309fbdb
seiiti <tseiiti@gmail.com>
2026-01-23 17:15:21 -0300

    config

--- hymn.js
+++ hymn.js
@@ -22,32 +22,16 @@ class Uta {
     "koto"
   ]
 
-  constructor(id, tamanho, espaco) {
-    this.id      = id
-    this.hymn    = HYMNS[`hymn_${ this.id }`]
-    this.tamanho = tamanho
-    this.espaco  = espaco
-    this.largura = 0
-    this.regexs  = []
+  constructor(hymn_id, font_size, space_width) {
+    this.hymn_id     = hymn_id
+    this.hymn        = HYMNS[`hymn_${ this.hymn_id }`]
+    this.font_size   = font_size
+    this.space_width = space_width
+    this.regexs      = []
     this.phrase
-    this.first = true
+    this.first
 
     for (let i in Uta.SEARCHES) { this.regexs.push(new RegExp(`^${ Uta.SEARCHES[i] }`)) }
-  }
-  
-  // cria lista de link dos hinos
-  get_links() {
-    let html = ''
-    for (let key in HYMNS) {
-      let aux = key == 'hymn_' + this.id ? 'active' : ''
-      html += `
-        <a class="dropdown-item px-2 menu-hymn menu-params ${ aux }"
-          href="javascript:conf.set_hymn('${ key.replace('hymn_', '') }')">
-            ${ HYMNS[key].title }
-        </a>
-        <hr class="dropdown-divider m-0">`
-    }
-    return html
   }
 
   // primeiro span com 3 pontos
@@ -87,6 +71,7 @@ class Uta {
     return text
   }
 
+  // ícones instrumentos
   get_narimono(line, index) {
     let html = ''
     for (let key of Uta.INSTRUMENTS) {
@@ -100,6 +85,7 @@ class Uta {
     return html
   }
 
+  // sílaba - 2 partes da nota
   get_syllable_part(text, l, i, j, p, q, v) {
     if (l.size < p + q) return ''
     
@@ -123,85 +109,53 @@ class Uta {
     return html
   }
 
+  // linha
   get_line(line, i, j) {
     this.phrase = (line.phrase || '').replace(/ /g, '')
     let html = `<div class="line line_${ j } ${ line.pause ? 'pause' : '' }">`
     html += this.get_first_syllable(i, j)
     if (line.pause) this.first = true
 
-    let id1 = 0
-    let id2 = 0
+    let i1 = 0
+    let i2 = 0
     while (this.phrase.length > 0) {
       let text = this.get_syllable_text()
       if (!text) {
         console.log(this.phrase)
         return
       }
-      html += `<div class="syllable syllable_${ id2 / 2 }">`
-      html += this.get_syllable_part(text, line, i, j, 1, id2, line.inverse)
+      html += `<div class="syllable syllable_${ i2 / 2 }">`
+      html += this.get_syllable_part(text, line, i, j, 1, i2, line.inverse)
       text = ''
-      if (line.halfs && line.meios.indexOf(id1 + 1) != -1) {
+      if (line.halfs && line.halfs.indexOf(i1 + 1) != -1) {
         text = this.get_syllable_text()
-        id1 += 1
+        i1 += 1
       }
-      html += this.get_syllable_part(text, line, i, j, 2, id2, line.inverse)
-      id1 += 1
-      id2 += 2
+      html += this.get_syllable_part(text, line, i, j, 2, i2, line.inverse)
+      i1 += 1
+      i2 += 2
       html += '</div>'
     }
     html += '</div>'
     return html
   }
 
+  // parágrafo
   get_paragraph(paragraph, i, size) {
     let html = `<div class="paragraph paragraph_${ i }">`
     for (let j in paragraph) {
       let line = paragraph[j]
       html += this.get_line(line, i, j)
 
-      // html += `<div class="line line_${ j } ${ line.parar ? 'parar' : '' }">`
-  
-      // let datas = `data-paragraph="${ i }"
-      //   data-line="${ j }"
-      //   data-syllable="${ -1}"
-      //   data-part="${ 1 }"`
-
-      // // criar sílaba inicial
-      // html += `
-      // <div class="syllable d-none d-md-block">
-      //   <progress class="beat first-beat ${ this.first ? 'd-none' : '' }" ${ datas } value="0" max="5">
-      //   </progress>`
-      // if (this.first) {
-      //   // sílaba inicial de três pontos do hino
-      //   html += `
-      //     <span class="first-span paragraph_${ i } line_${ j }">
-      //       <span>.</span><span>.</span><span>.</span>
-      //     </span>`
-      //   this.first = false
-      // }
-      // html += '</div>'
-      // if (line.parar) this.first = true
-
-      // let aux = this.get_line(line, i, j)
-      // if (j < paragraph.length - 1 || !line.parar) {
-      //   let div = document.createElement('div')
-      //   div.innerHTML = aux
-      //   let e = div.querySelector('.syllable:last-child span:last-child progress')
-      //   if (e) e.classList.remove('beat')
-      //   html += div.innerHTML
-      // } else {
-      //   html += aux
-      // }
-      
-      // if (line.mensagem) {
-      //   // criar parágrafo de mensagem sepadora
-      //   html += `
-      //     </div><div class="line" style="margin-left: ${ this.tamanho * this.espaco * 2.5 }px;">
-      //     <div class="border-bottom mb-4 px-1 mensagem">
-      //       <p class="text-end fst-italic fw-light m-1">${line.mensagem}</p>
-      //     </div>`
-      // }
-      // html += '</div>'
+      if (line.message) {
+        // parágrafo de mensagem sepadora
+        html += `
+          <div class="line" style="margin-left: calc(var(--size) * 2.3);">
+            <div class="border-bottom mb-4 px-1 message" style="width: calc(var(--size) * ${ size });">
+              <p class="text-end fst-italic fw-light m-1">${line.message}</p>
+            </div>
+          </div>`
+      }
     }
     html += '</div>'
     return html
@@ -215,142 +169,6 @@ class Uta {
       let size = this.hymn.size
       html += this.get_paragraph(paragraph, i, size)
     }
-
-    // // ajuste da largura da mensagem
-    // let div = document.createElement('div')
-    // div.innerHTML = html
-    // div.querySelectorAll('.mensagem').forEach(e => { e.style.width = `${this.largura}px` })
-    // html = div.innerHTML
-  
-    return html
-  }
-
-  get_syllable_part_old(syllable, classes, narimono) {
-    let html    = ''
-    if (syllable == '_')  syllable = ''
-    if (syllable == 'xi') syllable = 'i'
-    if (syllable == 'xo') syllable = 'o'
-    let datas = `data-paragraph="${ classes[2][0] }"
-      data-line="${ classes[2][1] }"
-      data-syllable="${ classes[2][2] }"
-      data-part="${ classes[2][3] }"`
-
-    html += `<span class="part ${ classes[0].join(' ').trim() }">`
-    html += `<progress class="${ classes[1].join(' ').trim() }" ${ datas } value="0" max="5">`
-    html += `</progress><div class="part_text">${ syllable }</div>`
-    html += narimono
-    html += '</span>'
-
-    return html
-  }
-
-  get_line_old(line, i, j) {
-    this.phrase = (line.phrase || '').replace(/ /g, '')
-    let html   = ''
-
-    let id1 = 0
-    let id2 = 0
-    while (this.phrase.length > 0) {
-      let syllable = this.get_syllable_text()
-      if (!syllable) {
-        console.log(this.phrase)
-        return
-      }
-      html += `<div class="syllable syllable_${ id2 / 2 }">`
-      
-      let classes = [ 
-        [
-          [ `part_${ line.inverso ? '2' : '1' }`, line.fim && line.fim == id1 ? 'fim' : null ], 
-          [ line.fim && line.fim == id1 ? null : `beat beat_${ id2 + 1 }` ], 
-          [ i, j, id2 / 2, 1 ], 
-        ], [
-          [ `part_${ line.inverso ? '1' : '2' }`, line.fim && line.fim == id1 ? 'd-none' : null ], 
-          [ `beat beat_${ id2 + 2 }` ], 
-          [ i, j, id2 / 2, 2 ]
-        ], 
-      ]
-
-      html += this.get_syllable_part(syllable, classes[0], this.get_narimono(line, id2))
-      if (line.meios && line.meios.indexOf(id1 + 1) != -1) {
-        syllable = this.get_syllable_text(this.phrase)
-        html += this.get_syllable_part(syllable, classes[1], this.get_narimono(line, id2 + 1))
-        id1 += 1
-      } else {
-        html += this.get_syllable_part('', classes[1], this.get_narimono(line, id2 + 1))
-      }
-      id1 += 1
-      id2 += 2
-      html += '</div>'
-    }
-
-    let aux = id2 * this.tamanho * this.espaco
-    if (j > 0) aux = (id2 + 3) * this.tamanho * this.espaco
-    if (aux > this.largura) this.largura = aux
-    
-    return html
-  }
-
-  get_hymn_html_old() {
-    let primeiro = true
-    let html = `<h1>${ this.hymn.title }</h1>\n`
-    for (let i in this.hymn.paragraphs) {
-      let paragraph = this.hymn.paragraphs[i]
-      html += `<div class="paragraph paragraph_${ i }">`
-      for (let j in paragraph) {
-        let line = paragraph[j]
-        html += `<div class="line line_${ j } ${ line.parar ? 'parar' : '' }">`
-    
-        let datas = `data-paragraph="${ i }"
-          data-line="${ j }"
-          data-syllable="${ -1}"
-          data-part="${ 1 }"`
-
-        // criar sílaba inicial
-        html += `
-        <div class="syllable d-none d-md-block">
-          <progress class="beat first-beat ${ primeiro ? 'd-none' : '' }" ${ datas } value="0" max="5">
-          </progress>`
-        if (primeiro) {
-          // sílaba inicial de três pontos do hino
-          html += `
-            <span class="first-span paragraph_${ i } line_${ j }">
-              <span>.</span><span>.</span><span>.</span>
-            </span>`
-          primeiro = false
-        }
-        html += '</div>'
-        if (line.parar) primeiro = true
-
-        let aux = this.get_line(line, i, j)
-        if (j < paragraph.length - 1 || !line.parar) {
-          let div = document.createElement('div')
-          div.innerHTML = aux
-          let e = div.querySelector('.syllable:last-child span:last-child progress')
-          if (e) e.classList.remove('beat')
-          html += div.innerHTML
-        } else {
-          html += aux
-        }
-        
-        if (line.mensagem) {
-          // criar parágrafo de mensagem sepadora
-          html += `
-            </div><div class="line" style="margin-left: ${ this.tamanho * this.espaco * 2.5 }px;">
-            <div class="border-bottom mb-4 px-1 mensagem">
-              <p class="text-end fst-italic fw-light m-1">${line.mensagem}</p>
-            </div>`
-        }
-        html += '</div>'
-      }
-      html += '</div>'
-    }
-
-    // ajuste da largura da mensagem
-    let div = document.createElement('div')
-    div.innerHTML = html
-    div.querySelectorAll('.mensagem').forEach(e => { e.style.width = `${this.largura}px` })
-    html = div.innerHTML
-  
     return html
   }
 }
--- index.js
+++ index.js
@@ -1,148 +1,116 @@
 class Config {
   constructor() {
-    let aux = localStorage.getItem('control')
-    aux = aux ? JSON.parse(aux) : {}
-    this.control         = {}
-    this.control.id = aux.id || '0'
-    this.control.tamanho = aux.tamanho || 16
-    this.control.espaco  = aux.espaco  || 1.4
-    this.control.instrus = aux.instrus || {}
-    this.control.bpm     = aux.bpm     || 60
-    this.control.anima   = aux.anima
-    this.control.qtd_s   = aux.qtd_s   || [ 21, 3, 3 ]
+    let aux = JSON.parse(localStorage.getItem('control') || '{}')
+    this.control = {}
+    this.control.hymn_id = aux.hymn_id || '0'
+    this.control.font_size = aux.font_size || 16
+    this.control.space_width = aux.space_width  || 1.3
+    this.control.bpm_time = aux.bpm_time || 60
+    this.control.suwari_0 = aux.suwari_0 || 21
+    this.control.suwari_1 = aux.suwari_1 || 3
+    this.control.suwari_2 = aux.suwari_2 || 3
+    this.control.scroll = !(aux.scroll == false)
+    this.control.animation = !(aux.animation == false)
+    this.control.instruments = aux.instruments || {}
+    this.fill = new Fill(this)
+    this.hymn
 
-    this.bpm(this.control.bpm)
-    this.anima(this.control.anima)
-    this.quant(0, this.control.qtd_s[0])
-    this.quant(1, this.control.qtd_s[1])
-    this.quant(2, this.control.qtd_s[2])
+    qs('.bpm_time').value = this.control.bpm_time
+    qs('.suwari_0').value = this.control.suwari_0
+    qs('.suwari_1').value = this.control.suwari_1
+    qs('.suwari_2').value = this.control.suwari_2
+    qs('.scroll').checked = this.control.scroll
+    qs('.animation').checked = this.control.animation
     qs('.menu-instruments').innerHTML = this.get_instruments() + qs('.menu-instruments').innerHTML
     
-    this.hino
-    this.fill
     this.set_hymn()
   }
 
-  set_hymn(id) {
+  set_hymn(hymn_id) {
     clearInterval(this.fill.interval)
     this.fill.track_icon(0)
 
-    if (id != null) this.control.id = id
-    this.hino = new Uta(this.control.id, this.control.tamanho, this.control.espaco)
+    if (hymn_id != null) this.control.hymn_id = hymn_id
+    this.hymn = new Uta(this.control.hymn_id, this.control.font_size, this.control.space_width)
 
-    qs('.menu-hymns li').innerHTML = this.hino.get_links()
-    qs('main').innerHTML = this.hino.get_hymn_html()
-    qs('.fixed-bottom span.text-capitalize').innerHTML = ` ${ this.hino.hymn.title }`
+    qs('.menu-hymns li').innerHTML = this.get_links()
+    qs('main').innerHTML = this.hymn.get_hymn_html()
+    qs('.fixed-bottom span.text-capitalize').innerHTML = ` ${ this.hymn.hymn.title }`
 
-    // if (this.control.id == 's') {
-    //   let es = qsa('.mensagem p')
-    //   es[0].innerText = `0 de ${ this.control.qtd_s[0] } vezes`
-    //   es[1].innerText = `0 de 1 vez`
-    //   es[2].innerText = `0 de ${ this.control.qtd_s[1] } vezes ( de 0 de ${ this.control.qtd_s[2] } )`
-    // }
+    if (this.control.hymn_id == 's') {
+      let es = qsa('.message p')
+      es[0].innerText = `0 de ${ this.control.suwari_0 } vezes`
+      es[1].innerText = `0 de 1 vez`
+      es[2].innerText = `0 de ${ this.control.suwari_1 } vezes ( de 0 de ${ this.control.suwari_2 } )`
+    }
 
-    for (let chave of Uta.INSTRUMENTS) {
-      if (this.control.instrus[chave]) {
-        qs(`input[name=${ chave }]`).checked = true
-        this.instrument(qs(`input[name=${ chave }]`))
+    for (let key of Uta.INSTRUMENTS) {
+      if (this.control.instruments[key]) {
+        qs(`input[name=${ key }]`).checked = true
+        this.instrument(qs(`input[name=${ key }]`))
       }
     }
-    this.tamanho(this.control.tamanho)
-    this.espaco(this.control.espaco)
-
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth"
-    })
+    
+    this.set('font_size', this.control.font_size)
+    this.set('space_width', this.control.space_width)
+    
+    if (this.control.scroll)
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
 
     this.fill = new Fill(this)
   }
 
-  set(chave, valor) {
-    this.control[chave] = valor
+  set(key, val) {
+    this.control[key] = val
     localStorage.setItem('control', JSON.stringify(this.control)) 
+
+    if ([ 'font_size', 'space_width' ].includes(key)) {
+      if (key == 'font_size') this.property('size-base', `${ val }px`)
+      if (key == 'space_width') {
+        this.property('size-width-multiply', val)
+        val = val.toFixed(2)
+      }
+      qs(`.${ key }`).innerText = val
+    }
   }
 
-  get(chave) {
-    return this.control[chave]
+  get(key) {
+    return this.control[key]
+  }
+
+  // ajuste + e -
+  shift(e) {
+    this.set(e.name, this.get(e.name) + Number(e.dataset.val))
+  }
+
+  // ajuste
+  change(e) {
+    let val = Number(e.value)
+    if (val > 0) this.set(e.name, val)
+  }
+
+  // ajuste radio button
+  toggle(e) {
+    this.set(e.name, e.checked)
   }
 
   // altera propriedade css
-  property(chave, valor) {
+  property(key, val) {
     let root = document.documentElement
-    root.style.setProperty(`--${chave}`, valor)
-  }
-
-  // altera tamanho da fonte via css
-  tamanho(aux) {
-    if (aux > 2) {
-      this.property('size-base', `${ aux }px`)
-      this.set('tamanho', aux)
-      qs('.font_size').innerHTML = aux // precisa de classe tamanho
-    }
-  }
-
-  // altera largura da sílaba via css
-  espaco(aux) {
-    if (aux > 0.8) {
-      this.property('size-width-multiply', aux.toFixed(2))
-      this.set('espaco', aux)
-      qs('.space_width').innerHTML = aux.toFixed(2) // precisa de classe space_width
-    }
-  }
-
-  // define tempo de batidas por minuto
-  bpm(aux) {
-    if (aux > 0) {
-      this.set('bpm', aux)
-      qs('.bpm_time').value = aux // precisa de classe bpm
-    }
-  }
-
-  // define animação
-  anima(aux) {
-    this.set('anima', aux)
-    qs('.animation').checked = aux
-    this.fill = new Fill(this)
-  }
-
-  // define quantidade de suwarizutome - 1a e 3a parte
-  quant(i, aux) {
-    if (aux > 0) {
-      aux = parseInt(aux)
-      let qtd_s = this.get('qtd_s')
-      qtd_s[i] = aux
-      this.set('qtd_s', qtd_s)
-      qs(`.suwari_${i}`).value = aux // precisa de classes suwari_*
-    }
-  }
-
-  // cria lista de checkbox de instrumentos
-  get_instruments() {
-    let html = ''
-    for (let chave of Uta.INSTRUMENTS) {
-      html += `
-        <li class="dropdown-item form-check ps-4">
-          <input type="checkbox" class="form-check-input instrument" name="${ chave }" 
-            id="check_${ chave }" onchange="conf.instrument(this)">
-          <label class="form-check-label" for="check_${ chave }">${ chave }</label>
-        </li>`
-    }
-    return html
+    root.style.setProperty(`--${key}`, val)
   }
 
   // alterna visualização de ícones de instrumentos
-  instrument_icon(chave, aux) {
-    qs(`input[name=${ chave }]`).checked = aux
-    qsa(`.${ chave }`).forEach(e => {
+  instrument_icon(key, aux) {
+    qs(`input[name=${ key }]`).checked = aux
+    qsa(`.${ key }`).forEach(e => {
       if (aux) {
         e.classList.remove('d-none')
       } else {
         e.classList.add('d-none')
       }
     })
-    this.control.instrus[chave] = aux
+    this.control.instruments[key] = aux
     localStorage.setItem('control', JSON.stringify(this.control))
   }
 
@@ -150,9 +118,9 @@ class Config {
   instrument(c) {
     this.instrument_icon(c.name, c.checked)
     let count = 0
-    qsa('.instrument').forEach(e => { if (e.checked) count += 1 }) // depende de .instrument
-    qs('#chk_all').checked  = count == Uta.INSTRUMENTS.length // depende de #chk_all
-    qs('#chk_none').checked = count == 0 // depende de #ch_nenhum
+    qsa('.instrument').forEach(e => { if (e.checked) count += 1 })
+    qs('#chk_all').checked  = count == Uta.INSTRUMENTS.length
+    qs('#chk_none').checked = count == 0
   }
 
   // execução de todos instrument_icon
@@ -162,6 +130,35 @@ class Config {
       e.checked = c.checked
       this.instrument_icon(e.name, e.checked)
     })
+  }
+  
+  // cria lista de link dos hinos
+  get_links() {
+    let html = ''
+    for (let key in HYMNS) {
+      let aux = key == 'hymn_' + this.hymn_id ? 'active' : ''
+      html += `
+        <a class="dropdown-item px-2 menu-hymn menu-params ${ aux }"
+          href="javascript:conf.set_hymn('${ key.replace('hymn_', '') }')">
+            ${ HYMNS[key].title }
+        </a>
+        <hr class="dropdown-divider m-0">`
+    }
+    return html
+  }
+
+  // cria lista de checkbox de instrumentos
+  get_instruments() {
+    let html = ''
+    for (let key of Uta.INSTRUMENTS) {
+      html += `
+        <li class="dropdown-item form-check ps-4">
+          <input type="checkbox" class="form-check-input instrument" name="${ key }" 
+            id="check_${ key }" onchange="conf.instrument(this)">
+          <label class="form-check-label" for="check_${ key }">${ key }</label>
+        </li>`
+    }
+    return html
   }
 
 }
@@ -181,7 +178,7 @@ class Fill {
     this.qs_3     = 0
     this.beats   = qsa('progress.beat')
     this.t_len    = qsa('progress.beat:not(.d-none)').length
-    this.anima    = this.conf.get('anima')
+    this.animation    = this.conf.get('animation')
   }
 
   cg(p) {
@@ -197,12 +194,11 @@ class Fill {
       e.classList.add('fw-bold')
     } else {
       e.classList.remove('fw-bold')
-      // e.classList.add('fw-light')
     }
   }
 
   enfase(f) {
-    if (!this.anima) return
+    if (!this.animation) return
     let e = this.beats[this.atual]
     if (!e) return
     e = e.parentElement.querySelector('.part_text')
@@ -237,9 +233,9 @@ class Fill {
     this.track_icon(1)
     let e = this.beats[this.atual]
     let es = qsa(`.first-span.paragraph_${e.dataset.paragraph}.line_${e.dataset.line} span`)
-    es[0].scrollIntoView({ behavior: "smooth", block: 'center', inline: "nearest" })
+    es[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
     
-    if (this.anima) es[0].style.color = '#555'
+    if (this.animation) es[0].style.color = '#555'
     let i = 1
     this.interval = setInterval(function(fx) {
       if (i == 3) {
@@ -249,9 +245,9 @@ class Fill {
         return
       }
       e = es[i]
-      if (fx.anima) e.style.color = '#555'
+      if (fx.animation) e.style.color = '#555'
       i += 1
-    }, 1000 * 60 / this.cg('bpm'), this);
+    }, 1000 * 60 / this.cg('bpm_time'), this)
   }
 
   play_suwari_aux_1() {
@@ -312,7 +308,7 @@ class Fill {
     let e = this.beats[this.atual]
     if (!e) return
     let lin = qs(`.paragraph_${e.dataset.paragraph} .line_${e.dataset.line}`)
-    lin.scrollIntoView({ behavior: "smooth", block: 'center', inline: "nearest" })
+    lin.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
     let es = lin.querySelectorAll('.beat')
     if (es[es.length - 1] == e) {
       this.enfase(false)
@@ -332,7 +328,7 @@ class Fill {
 
     let e = this.beats[this.atual]
     qsa(`.first-span.paragraph_${e.dataset.paragraph}.line_${e.dataset.line} span`)
-      .forEach(f => { if (this.anima) f.style.color = '#555' })
+      .forEach(f => { if (this.animation) f.style.color = '#555' })
 
     if (this.cg('id') == 's') {
       let es = qsa('.mensagem p')
@@ -345,14 +341,14 @@ class Fill {
     if (e.classList.contains('d-none')) this.atual += 1
     this.enfase(true)
 
-    let delay = 100 * 60 / this.cg('bpm')
+    let delay = 100 * 60 / this.cg('bpm_time')
     this.interval = setInterval(function(fx) {
       let e = fx.beats[fx.atual]
       if (!e) {
         fx.track_icon(3)
         return
       }
-      if (fx.anima) e.value = fx.valor
+      if (fx.animation) e.value = fx.valor
 
       fx.valor += 1
       if (fx.valor > 5) {
@@ -362,7 +358,7 @@ class Fill {
 
         fx.play_aux()
       }
-    }, delay, this);
+    }, delay, this)
   }
 
   reset() {
